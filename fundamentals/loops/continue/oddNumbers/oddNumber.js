fe7f44a9132c3c3a132fef70dfc2886438c5f3b8
sajwan05 <surajsajwan328@gmail.com>
2026-08-08 17:48:09 +0530

    generate odd numbers using continue

---
+++ fundamentals/loops/continue/oddNumbers/oddNumber.js
@@ -0,0 +1,11 @@
+// return odd number using continue
+
+let navigation = 10;
+for(let i = 1; i <= navigation; i++){
+    if(i % 2){
+        console.log(i);
+    }else {
+        continue;
+    }
+}
+
