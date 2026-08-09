ebe089c345db43e95633b8ba30397f85efda807a
sajwan05 <surajsajwan328@gmail.com>
2026-08-09 16:54:31 +0530

    array

--- fundamentals/arrays/arrays.js
+++ fundamentals/arrays/arrays.js
@@ -211,3 +211,24 @@
     console.log(matrix[1][1]);
 }
 
+// toString
+
+{
+    const arr = [1, 2, 3];
+
+    console.log(arr);
+    console.log(String(arr) === "1,2,3");
+
+    console.log([] + 1);
+    console.log([1] + 1);
+    console.log([1, 2] + 1);
+
+    console.log([] == []);
+    console.log([0] == [0]);
+
+    console.log(0 == []);
+    console.log('0' == []);
+}
+
+
+
