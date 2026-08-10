7e450198721c5c832d484883efb7077fefc35f22
sajwan05 <surajsajwan328@gmail.com>
2026-08-10 23:08:11 +0530

    learn splice array method which is kind of swiss army knife

---
+++ fundamentals/arrays/array-methods/arrMethod.js
@@ -0,0 +1,68 @@
+// Array methods
+// Add remove items
+
+// splice
+
+{
+    let arr = ["I", "go", "home"];
+
+    // arrays are basically objects, so we can use delete 
+
+    console.log(delete arr[1]);
+
+    console.log(arr);
+
+    console.log(arr[1]);
+
+    console.log(arr.length);
+}
+
+// syntax: arr.splice(start[, deletecount, elem1, ..., elemN ]); return array of the removed elements
+
+{
+    const studies = ["I", "study", "Javascript"];
+    console.log(studies);
+
+    const splicedArray = studies.splice(1, 1);
+    console.log(splicedArray);
+    console.log(studies);
+}
+
+{
+    // I study js rn ---> let's dance right now
+
+    const sentence = ["I", "Study", "Javascript", "right", "now"];
+    console.log(sentence);
+
+    let splicedArr = sentence.splice(0, 3, 'Let\'s', "dance");
+    console.log(splicedArr);
+
+    console.log(sentence);
+
+    sentence.splice(0, 2, "I", "study", "JavaScript");
+
+    console.log(sentence);
+
+    const removed = sentence.splice(0, 2);
+    console.log(removed);
+    console.log(sentence);
+}
+
+{
+    const arr = ["I", "Study", "JavaScript"];
+
+    const spliced = arr.splice(2, 0, "complex", "language");
+
+    console.log(arr);
+    console.log(spliced);
+}
+
+// Negative indexes allowed
+
+{
+    const numbers = [1, 2, 5];
+
+    numbers.splice(-1, 0, 3, 4);
+
+    console.log(numbers);
+}
