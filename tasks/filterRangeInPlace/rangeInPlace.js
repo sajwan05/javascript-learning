744d58acca44cbaaf0dfb3d8aee2305ba752031a
sajwan05 <surajsajwan328@gmail.com>
2026-08-19 10:56:24 +0530

    filterInPlace for a range using forEach ofcourse wrong and then for loop ofcourse right

---
+++ tasks/filterRangeInPlace/rangeInPlace.js
@@ -0,0 +1,46 @@
+/*
+Pseudocode: 
+    first we will iterate through the original array
+    then for each element we will check if it is not between the given range 
+    if not so we will remove it with some array method
+
+*/ 
+
+{
+    function getFilterRangeInPlace(arr, min, max){
+
+        arr.forEach((item, index) => {
+            if(item < min || item > max){
+                arr.splice(index, 1);
+            }
+
+            
+        })
+
+        console.log(arr);
+    }
+
+    getFilterRangeInPlace([5, 3, 8, 1], 1, 4);
+    getFilterRangeInPlace([2, 4, 6, 7, 8, 10, 34, 54, 65], 1, 40);
+}
+
+{
+    function getFilterRangeInPlace(arr, min, max){
+
+        for(let i = 0; i < arr.length; i++){
+
+            if(arr[i] < min || arr[i] > max){
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+
+        console.log(arr);
+    }
+
+    getFilterRangeInPlace([5, 3, 8, 1], 1, 4);
+    getFilterRangeInPlace([2, 4, 6, 7, 8, 10, 34, 54, 65], 1, 40);
+    getFilterRangeInPlace([2, 4, 6, 7, 8, 10, 34, 54, 65], 11, 40);
+    getFilterRangeInPlace([2, 4, 6, 7, 8, 10, 34, 54, 65], 6, 34);
+}
+
