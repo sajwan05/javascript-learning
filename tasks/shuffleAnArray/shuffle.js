5f051522c7d72910363b5a50bf537de58128e045
sajwan05 <surajsajwan328@gmail.com>
2026-08-19 13:06:40 +0530

    wrote robust solution using fischer yates algo

--- tasks/shuffleAnArray/shuffle.js
+++ tasks/shuffleAnArray/shuffle.js
@@ -59,3 +59,75 @@ then i will return the shuffled array
 }
 
 
+{
+    function shuffle(arr){
+
+        for(let i = arr.length - 1; i >= 0; i--){
+            const rndIndex = Math.floor(Math.random() * (i + 1));
+
+            [arr[i], [arr[rndIndex]]] = [arr[rndIndex], arr[i]];
+        }
+
+        return arr;
+    }
+
+    {
+    function shuffleArr(array){
+        
+        for(let i = 0; i < array.length; i++){
+            const randomIndex = Math.floor(Math.random() * array.length);
+
+            array[i] = array[randomIndex];
+        }
+
+        return array;
+    }
+
+    // console.log(shuffleArr([1, 2, 3]));
+
+    // I was approaching it all wrong!
+}
+
+/*
+Pseudocode:
+
+first i will iterate through the given arrray
+then i will generate random index 
+then i will swap random index with already itrable index and tada 
+then i will return the shuffled array
+*/ 
+
+{
+    function shuffleArr(array){
+
+        const length = array.length;
+        for(let i = 0; i < length; i++){
+            const randomIndex = Math.floor(Math.random() * length);
+
+            const temporary = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temporary;
+        }
+
+        return array;
+    }
+
+    console.log(shuffleArr([1, 2, 3]));
+}
+
+{
+    function shuffleArr(array){
+
+        for(let i = 0; i < array.length; i++){
+
+            const randIndex = Math.floor(Math.random() * (i + 1));
+
+            [array[i], array[randIndex]] = [array[randIndex], array[i]];
+        }
+
+        return array;
+    }
+
+    console.log(shuffleArr([1, 2, 3]));
+}
+}
