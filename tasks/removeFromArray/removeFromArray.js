c9f5fd5e0f52e71c6583bc8add894d2c5389c569
sajwan05 <surajsajwan328@gmail.com>
2026-08-20 22:00:53 +0530

    alternative solution

--- tasks/removeFromArray/removeFromArray.js
+++ tasks/removeFromArray/removeFromArray.js
@@ -31,3 +31,22 @@ then we will get the resultant array
 
     console.log(removeFromAnArray2([2, 3, 5, 6, 7, 8, 9, 11], 3, 7, 9));
 }
+
+// There can be more solution like without using splice
+
+{
+    function removeFromAnArray3(array, ...args){
+        const newArray = [];
+
+        for(let i = 0; i < array.length; i++){
+            if(!args.includes(array[i])){
+                newArray.push(array[i]);
+            }
+        }
+
+        return newArray;
+    }
+
+    console.log(removeFromAnArray3([2, 3, 5, 6, 7, 8, 9, 11], 3, 7, 9));
+    console.log(removeFromAnArray3([1, 2, 3, 4, 5, 6, 7, "hello", "you"], 1, "hello", "you"));
+}
