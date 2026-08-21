e12a833bd638b9c8af45fa993406f21ad89be1fa
sajwan05 <surajsajwan328@gmail.com>
2026-08-21 14:52:56 +0530

    syntax without and with destructuring

---
+++ fundamentals/arrays/arrayDestructuring/arrDestructuring.js
@@ -0,0 +1,18 @@
+// The syntax with destructuring 
+{
+    const [first, second, third] = ["Laide", "Gabriel", "Jets"];
+
+    console.log(first);
+    console.log(second);
+    console.log(third);
+}
+
+
+// The syntax without destructuring
+{
+    const first = "Laide",
+      second = "Gabriel",
+      third = "Jets";
+
+      console.log(third);
+}
