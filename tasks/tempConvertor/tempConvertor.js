1a7f9db4eb92f90367d5e12b1d27e9089f8ba8a5
sajwan05 <surajsajwan328@gmail.com>
2026-08-21 22:53:57 +0530

    temp conversion alternative way

--- tasks/tempConvertor/tempConvertor.js
+++ tasks/tempConvertor/tempConvertor.js
@@ -21,3 +21,29 @@
     console.log(getTempInFahrenheit(100));
     console.log(getTempInFahrenheit(0));
 }
+
+{
+
+    function getFahrenheitToCelsius(tempInF){
+        let tempInC = (tempInF - 32.0) * 5 / 9;
+
+        return parseFloat((tempInC).toFixed(1))
+    }
+
+    console.log(getFahrenheitToCelsius(98.4));
+    console.log(getFahrenheitToCelsius(76));
+    console.log(getFahrenheitToCelsius(32));
+
+}
+
+{
+    function getTempInFahrenheit(tempInC){
+        let tempInF = tempInC * (9 / 5) + 32;
+
+        return parseFloat((tempInF).toFixed(1));
+    }
+
+    console.log(getTempInFahrenheit(36.9));
+    console.log(getTempInFahrenheit(100));
+    console.log(getTempInFahrenheit(0));
+}
