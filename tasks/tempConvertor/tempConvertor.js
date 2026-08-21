4a5ae1209e16fbf2341a224dff54bbb5a09ce38e
sajwan05 <surajsajwan328@gmail.com>
2026-08-21 22:49:50 +0530

    temp converotor

---
+++ tasks/tempConvertor/tempConvertor.js
@@ -0,0 +1,23 @@
+{
+    function getFahrenheitToCelsius(tempInF){
+        let tempInC = (tempInF - 32.0) * 5 / 9;
+
+        return Math.round(tempInC * 10) / 10;
+    }
+
+    console.log(getFahrenheitToCelsius(98.4));
+    console.log(getFahrenheitToCelsius(76));
+    console.log(getFahrenheitToCelsius(32));
+}
+
+{
+    function getTempInFahrenheit(tempInC){
+        let tempInF = tempInC * (9 / 5) + 32;
+
+        return Math.round(tempInF * 10) / 10;
+    }
+
+    console.log(getTempInFahrenheit(36.9));
+    console.log(getTempInFahrenheit(100));
+    console.log(getTempInFahrenheit(0));
+}
