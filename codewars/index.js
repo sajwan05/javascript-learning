40822ec1e663883cf65127933288a0329193520d
sajwan05 <surajsajwan328@gmail.com>
2026-08-23 22:58:38 +0530

    improved solution

--- codewars/index.js
+++ codewars/index.js
@@ -1,7 +1,22 @@
+// Date Aug 23 2026 -- time 10: 50 PM
+
 {
     function multiply(a, b){
         return a * b;
     }
 
     console.log(multiply(2, 3));
+}
+
+{
+    function multiply(a, b){
+        if(!a || !b || typeof a !== "number" || typeof b !== "number"){
+            throw new Error("Both argument should be number");
+        }else {
+            return a * b;
+        }
+    }
+
+    console.log(multiply(3, 6));
+    console.log(multiply(2, "3"));
 }
