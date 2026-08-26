16c877bb9824046f1db5aa2e51047eaf4b04d434
sajwan05 <surajsajwan328@gmail.com>
2026-08-26 12:10:21 +0530

    learn to use all 3 ways to handle events in js dom

---
+++ dom/dom-odin/index.js
@@ -0,0 +1,21 @@
+console.log("Javascript is connected..");
+// method 2
+const button = document.querySelector("#btn");
+
+button.onclick = () => console.log("You successfully clicked me!");
+
+// method 3
+
+const btn = document.querySelector(".method3");
+
+// function logOnClick(){
+//     console.log("You clicked me with method 3");
+// }
+
+// btn.addEventListener("click", logOnClick);
+
+// or
+
+btn.addEventListener("click", () => {
+    console.log("Hello, World!");
+});
