1b1eed48c704a6411f37f1f82e8a81ff400d33fa
sajwan05 <surajsajwan328@gmail.com>
2026-08-26 13:20:17 +0530

    build a random button color generator

---
+++ dom/dom-odin/random-color-btn/index.js
@@ -0,0 +1,54 @@
+// const button = document.querySelector(".random-color");
+
+// function random(number){
+//     return Math.floor(Math.random() * number);
+// }
+
+const button = document.querySelector(".random-color");
+console.log(button);
+
+
+function random(number){
+    return Math.floor(Math.random() * number);
+}
+
+function changeBgColor(e){
+    const randomColor = `rgb(${random(256)}, ${random(256)}, ${random(256)})`;
+
+    return e.target.style.backgroundColor = randomColor;
+}
+
+button.addEventListener("click", changeBgColor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const button = document.querySelector(".random-color");
+// console.log(button);
+
+// function random (number){
+//         return Math.floor(Math.random() * number);
+//     }
+
+// button.addEventListener("click", function(e){
+
+//     const randomColor = `rgb(${random(256)}, ${random(256)}, ${random(256)})`;
+
+//     const target = e.target;
+//     // console.log(target);
+//    target.setAttribute("style", "background: randomColor;");
+
+// })
