6f2238037a211e74984d0eea9be0061a1e6c1a4b
sajwan05 <surajsajwan328@gmail.com>
2026-08-31 23:22:38 +0530

    to know what extra keys are pressed

--- dom/assignments/mouseEvents/modifierKeys/index.js
+++ dom/assignments/mouseEvents/modifierKeys/index.js
@@ -1,1 +1,17 @@
 console.log("Javascript is key!");
+
+const btnKeys = document.querySelector(".btnKeys");
+
+
+btnKeys.addEventListener('click', (e) => {
+    let keys = [];
+
+    if(e.shiftKey) keys.push("shift");
+    if(e.ctrlKey) keys.push("ctrl");
+    if(e.altKey) keys.push("alt");
+    if(e.metaKey) keys.push("meta");
+
+    const output = document.querySelector("#messageKeys");
+
+    output.textContent = `Keys: ${keys.join('+')}`;
+});
