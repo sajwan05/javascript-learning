ff04bdf43dfe841b081eb2501761d8b56a06a66f
sajwan05 <surajsajwan328@gmail.com>
2026-09-02 11:18:27 +0530

    mouse location on screen

--- dom/assignments/mouseEvents/screen-coordinates/index.js
+++ dom/assignments/mouseEvents/screen-coordinates/index.js
@@ -1,1 +1,12 @@
 console.log("Javascript!");
+
+const track = document.querySelector(".track");
+const log = document.querySelector(".log")
+
+track.addEventListener("mousemove", (event) => {
+    
+    log.textContent = `
+        Screen X/Y: (${event.screenX}, ${event.screenY})
+        Client X/Y: (${event.clientX}, ${event.clientY})
+    `;
+});
