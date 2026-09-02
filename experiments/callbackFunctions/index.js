ae1fe8363ecf48e50ac3467f3accaad578b158c8
sajwan05 <surajsajwan328@gmail.com>
2026-09-02 17:43:31 +0530

    callbacks functions

--- experiments/callbackFunctions/index.js
+++ experiments/callbackFunctions/index.js
@@ -8,3 +8,39 @@ notes.forEach(function (note) {
 });
 
 notes.forEach(console.log);
+
+// How callbacks work
+
+function myForEach(array, callback){
+    for(let i = 0; i < array.length; i++){
+        callback(array[i]);
+    }
+}
+
+const myArray = [3, 5,7, 9];
+
+myForEach(myArray, (item) => {
+    console.log(item + 1);
+});
+
+
+// possibly map under the hood
+
+
+function myMap(array, callback){
+    const myNewArray = [];
+
+    for(let i = 0; i < array.length; i++){
+        const callbackResult = callback(array[i]);
+
+        myNewArray.push(callbackResult);
+    }
+
+    return myNewArray;
+}
+
+const addedArray = myMap([1, 2, 3, 4], (arrItem) => {
+    return arrItem + 2;
+})
+
+console.log(addedArray);
