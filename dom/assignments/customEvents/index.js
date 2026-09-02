c2a938a5099e6e7135f7b2b0f26dcbed32dd519f
sajwan05 <surajsajwan328@gmail.com>
2026-09-02 13:43:50 +0530

    custom events

--- dom/assignments/customEvents/index.js
+++ dom/assignments/customEvents/index.js
@@ -1,1 +1,15 @@
-console.log("Javascript!");
+const note = document.querySelector(".note");
+
+function highlight(elem, callback){
+    elem.style.backgroundColor = "yellow";
+
+    if(callback && typeof callback === "function"){
+        callback(elem);
+    }
+}
+
+function addBorder (elem){
+    elem.style.border = `2px solid red`;
+}
+
+highlight(note, addBorder);
