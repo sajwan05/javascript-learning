c05f33616c52853e4a20d857589804c30b26915e
sajwan05 <surajsajwan328@gmail.com>
2026-09-02 12:02:15 +0530

    use event delegation to handle click event on each child of menu

--- dom/assignments/eventDelegation/index.js
+++ dom/assignments/eventDelegation/index.js
@@ -13,3 +13,20 @@ dashboard.addEventListener("click", (event) => {
 report.addEventListener("click", (event) => {
     console.log("report menu was cliked!");
 });
+
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", (event) => {
+    let target = event.target;
+    
+    switch(target.classList.value){
+        case 'home':
+            console.log("home menu is clicked! via delegation");
+            break;
+        case 'dashboard':
+            console.log("event delegation: dashboard is clicked!");
+            break;
+        case 'report':
+            console.log("event delegation: report menu was clicked!");
+    }
+});
