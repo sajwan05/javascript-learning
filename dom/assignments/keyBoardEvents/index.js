28c6e68f2d6579f33f6e0d0773ae2762bf22d0de
sajwan05 <surajsajwan328@gmail.com>
2026-09-02 11:47:11 +0530

    key and code property of keyboard event

--- dom/assignments/keyBoardEvents/index.js
+++ dom/assignments/keyBoardEvents/index.js
@@ -13,3 +13,6 @@ input.addEventListener("keypress", (event) => {
     console.log("keypress event is fired");
 });
 
+input.addEventListener("keydown", (event) => {
+    console.log(`key = ${event.key}, code = ${event.code}`);
+});
