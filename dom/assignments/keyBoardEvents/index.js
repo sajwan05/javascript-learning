5fa5812e6aaaaf0683479e822527839f46474841
sajwan05 <surajsajwan328@gmail.com>
2026-09-02 11:39:19 +0530

    how to handle all 3 keyboard events

---
+++ dom/assignments/keyBoardEvents/index.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("#message");
+
+input.addEventListener("keydown", (event) => {
+    console.log("Keydown event fired");
+    console.log(event.key);
+});
+
+input.addEventListener("keyup", (event) => {
+    console.log("key up event is fired");
+});
+
+input.addEventListener("keypress", (event) => {
+    console.log("keypress event is fired");
+});
+
