5a38da93a8a969601b29a2116c461de07b530c82
sajwan05 <surajsajwan328@gmail.com>
2026-09-19 16:49:32 +0530

    shorter syntax for objext literal methods

--- fundamentals/objects/object-mdn/index.js
+++ fundamentals/objects/object-mdn/index.js
@@ -1,11 +1,33 @@
-const person = {
-    name: ["Bob", "Smith"],
-    age: 32,
-    bio: function() {
-        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
-    },
-    
-    introduceSelf: function () {
-        console.log(`Hi! I'm ${this.name[0]}.`);
-    },
-};
+{
+    const person = {
+        name: ["Bob", "Smith"],
+        age: 32,
+
+        bio: function() {
+            console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+        },
+        
+        introduceSelf: function () {
+            console.log(`Hi! I'm ${this.name[0]}.`);
+        },
+    };
+}
+// There is a shorter syntax:
+{
+    const person = {
+        name: ["Bob", "Smith"],
+        age: 32,
+
+        bio(){
+            console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+        },
+
+        introduceSelf(){
+            console.log(`Hi! I'm ${this.name[0]}.`);
+        },
+    };
+
+    console.log(person.age);
+    console.log(person.bio());
+    console.log(person.introduceSelf());
+}
