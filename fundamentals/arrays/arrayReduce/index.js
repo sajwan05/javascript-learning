dfe9e0ceb052b3322f4ae7d2c038eb457026d214
sajwan05 <surajsajwan328@gmail.com>
2026-09-21 16:21:52 +0530

    all about reduce

--- fundamentals/arrays/arrayReduce/index.js
+++ fundamentals/arrays/arrayReduce/index.js
@@ -17,8 +17,30 @@ console.log(totalPrice);
 
 {
     const totalPrice = items.reduce((total, current) => {
+        console.log(`Total: ${total}`);
+        console.log(`Item: ${current.price}`);
         return total + current.price;
     }, 0);
 
 console.log(totalPrice);
 }
+
+// array of people
+
+const people = [
+    { name: "Kyle", age: 26, }, 
+    { name: "John", age: 31},
+    { name: "Sally", age: 42},
+    { name: "Jill", age: 42},
+];
+
+const groupedById = people.reduce((groupedPeople, person) => {
+    let age = person.age;
+    if(groupedPeople[age] == null) groupedPeople[age] = []
+
+    groupedPeople[age].push(person);
+
+    return groupedPeople;
+}, {});
+
+console.log(groupedById);
