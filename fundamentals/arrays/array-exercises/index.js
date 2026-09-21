ee8ea4b0ac73e1cafec4f6d47acf06e5deca23ea
sajwan05 <surajsajwan328@gmail.com>
2026-09-21 21:13:06 +0530

    filter list of inventors for those who were born in 1500s

---
+++ fundamentals/arrays/array-exercises/index.js
@@ -0,0 +1,46 @@
+const inventors = [
+        { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+        { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+        { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+        { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+        { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+        { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+        { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+        { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+        { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+        { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+        { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+        { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+    ];
+
+// get the list of inventors those were born in 1500s
+// Pseudocode 
+// we will intialize an empty array
+// then we will iterate through the given array of objects
+//      then we will check if someone was born b/w 1499 to 1600 if yes
+//          we will push those item into new array
+// at last will return new array
+
+{
+    function bornIn1500s(array){
+
+        const fifteenthCenturyBorn = [];
+
+        for(const item of array){
+            if(item.year >= 1500 && item.year < 1600){
+                fifteenthCenturyBorn.push(item);
+            }
+        }
+
+        return fifteenthCenturyBorn;
+    }
+
+    console.log(bornIn1500s(inventors));
+}
+// you can use filter to achieve this
+
+{
+    const fifteenthCenturyBorn = inventors.filter(user => user.year < 1600 && user.year >= 1500);
+
+    console.log(fifteenthCenturyBorn);
+}
